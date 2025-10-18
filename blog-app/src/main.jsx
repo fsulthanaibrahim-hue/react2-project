88e1c88dae15f032c5b9032953a428af4b54ba23
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2025-10-17 17:46:32 -0700

    Week2

--- blog-app/src/main.jsx
+++ blog-app/src/main.jsx
@@ -1,18 +1,33 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+// // import { StrictMode } from 'react'
+// // import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// // import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+// // createRoot(document.getElementById('root')).render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// // )
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BlogProvider } from "./context/BlogContext";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BlogProvider>
+//       <App />
+//     </BlogProvider>
+//   </React.StrictMode>
+// );
+
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BlogProvider } from "./context/BlogContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
